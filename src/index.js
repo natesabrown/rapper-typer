0b381e33d76f49b3945fe8dabf1507a8fb9b7f9f
natesabrown <nateabrown@icloud.com>
2020-09-18 22:17:19 -0500

    react + firebase boilerplate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, compose } from "redux";
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer,
+} from "react-redux-firebase";
+
+import "firebase/auth";
+import firebase from "firebase/app";
+import fbConfig from './fb-config';
+
+const rrfConfig = {
+  userProfile: "users",
+  profileFactory: (userData, profileData, firebase) => {
+    const { user } = userData;
+    return {
+      averageWPM: 0,
+    };
+  },
+};
+
+firebase.initializeApp(fbConfig);
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+});
+
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
